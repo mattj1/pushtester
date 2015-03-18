9410c11a7875a20ad79c37381a94e463cfced83b
dmob <anurag.bhatnagar@dictionary.com>
2015-03-18 16:48:36 -0700

    stuff

--- pushtest/settings.py
+++ pushtest/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
